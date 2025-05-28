f7c749046083d25c8b95705cbacdc70412350ae4
Mikito1111 <159195171+mikito1111@users.noreply.github.com>
2025-05-28 14:23:38 -0600

    Fix: Restructurar para Vercel - mover app a public/ y actualizar package.json

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = (req, res) => {
     try {
-        const htmlPath = path.join(__dirname, '..', 'appconectada.html');
+        const htmlPath = path.join(__dirname, '..', 'public', 'index.html');
         const html = fs.readFileSync(htmlPath, 'utf8');
         res.setHeader('Content-Type', 'text/html');
         res.status(200).send(html);
